3ce9406e7bbf94c66e815bfe2dfe18d30b1c3795
ward <bonuswon@gmail.com>
2022-07-05 22:39:28 +0900

    Feat: set deployment-automation

--- src/main/java/com/ward/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/ward/springboot/config/auth/CustomOAuth2UserService.java
@@ -46,7 +46,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 attributes.getNameAttributeKey());
     }
 
-
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepository.findByEmail(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
