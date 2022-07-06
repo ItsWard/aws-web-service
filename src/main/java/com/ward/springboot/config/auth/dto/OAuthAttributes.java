dc745a719b299572842904970c6baf3007cc7e20
ward <bonuswon@gmail.com>
2022-07-06 17:13:49 +0900

    Update

--- src/main/java/com/ward/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/ward/springboot/config/auth/dto/OAuthAttributes.java
@@ -59,7 +59,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
