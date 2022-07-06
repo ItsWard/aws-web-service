f86e469a5f9f2093aff8b83a632f3d12967586bc
ward <bonuswon@gmail.com>
2022-07-07 03:17:36 +0900

    Complete Chapter 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(cat $PROJECT_NAME.pid)
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
