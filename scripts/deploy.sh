f82671e55692f72234c5e56fbf4522d0696270e2
ward <bonuswon@gmail.com>
2022-07-07 02:55:47 +0900

    Feat: deploy.sh CURRENT_PID change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(cat $PROJECT_NAME.pid)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
