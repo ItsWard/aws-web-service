e493cdbfa635eea22c125d7dde231c4e3973fda2
ward <bonuswon@gmail.com>
2022-07-06 21:21:31 +0900

    Update: pid(aws-web-service)

--- src/main/java/com/ward/springboot/Application.java
+++ src/main/java/com/ward/springboot/Application.java
@@ -3,6 +3,7 @@ package com.ward.springboot;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @SpringBootApplication //해당 어노테이션이 있는 부분부터 설정진행, 내장 WAS 톰캣 서버 작동 가능
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
 
     }
 }
